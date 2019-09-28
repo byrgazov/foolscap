a601cd9fc6706a38792bc14be37ceac15aadb26e
byrgazov <bw@handsdriver.net>
2019-09-28 10:24:45 +0800

    fix requirements

--- setup.py
+++ setup.py
@@ -1,73 +1,94 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Command
-
+import setuptools
 import versioneer
+
 
 commands = versioneer.get_cmdclass().copy()
 
-class Trial(Command):
+class Trial(setuptools.Command):
     description = "run trial"
     user_options = []
 
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         import sys
         from twisted.scripts import trial
         sys.argv = ["trial", "--rterrors", "foolscap.test"]
         trial.run()  # does not return
+
 commands["trial"] = Trial
 commands["test"] = Trial
 
-setup_args = {
-    "name": "foolscap",
-    "version": versioneer.get_version(),
-    "description": "Foolscap contains an RPC protocol for Twisted.",
-    "author": "Brian Warner",
-    "author_email": "warner-foolscap@lothar.com",
-    "url": "http://foolscap.lothar.com/trac",
-    "license": "MIT",
-    "long_description": """\
+
+def setup():
+    setuptools.setup(
+        name    = 'foolscap',
+        version = versioneer.get_version(),
+        description  = 'Foolscap contains an RPC protocol for Twisted.',
+        author       = 'Brian Warner',
+        author_email = 'warner-foolscap@lothar.com',
+        url     = 'http://foolscap.lothar.com/trac',
+        license = 'MIT',
+        long_description = """\
 Foolscap (aka newpb) is a new version of Twisted's native RPC protocol, known
 as 'Perspective Broker'. This allows an object in one process to be used by
 code in a distant process. This module provides data marshaling, a remote
 object reference system, and a capability-based security model.
 """,
-    "classifiers": [
-        "Development Status :: 3 - Alpha",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Topic :: Internet",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Distributed Computing",
-        "Topic :: System :: Networking",
-        "Topic :: Software Development :: Object Brokering",
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Operating System :: OS Independent',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3 :: Only',
+            'Topic :: Internet',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: System :: Distributed Computing',
+            'Topic :: System :: Networking',
+            'Topic :: Software Development :: Object Brokering',
         ],
-    "platforms": ["any"],
 
-    "package_dir": {"": "src"},
-    "packages": ["foolscap", "foolscap.slicers", "foolscap.logging",
-                 "foolscap.connections",
-                 "foolscap.appserver", "foolscap.test"],
-    "entry_points": {"console_scripts": [
-        "flogtool = foolscap.logging.cli:run_flogtool",
-        "flappserver = foolscap.appserver.cli:run_flappserver",
-        "flappclient = foolscap.appserver.client:run_flappclient",
-        ] },
-    "cmdclass": commands,
-    "install_requires": ["twisted[tls] >= 16.0.0", "pyOpenSSL", "future >= 0.17.1", "six >= 1.12.0"],
-    "extras_require": {
-        "dev": ["mock", "future >= 0.17.1", "txsocksx", "txtorcon >= 0.16.1", "txi2p >= 0.3.2"],
-        "socks": ["txsocksx"],
-        "tor": ["txtorcon >= 0.16.1"],
-        "i2p": ["txi2p >= 0.3.2"],
-        },
-}
+        platforms = ['any'],
 
-if __name__ == "__main__":
-    setup(**setup_args)
+        package_dir = {'': 'src'},
+        packages = ['foolscap',
+                    'foolscap.slicers',
+                    'foolscap.logging',
+                    'foolscap.connections',
+                    'foolscap.appserver',
+                    'foolscap.test'],
 
+        entry_points = {'console_scripts': [
+            'flogtool = foolscap.logging.cli:run_flogtool',
+            'flappserver = foolscap.appserver.cli:run_flappserver',
+            'flappclient = foolscap.appserver.client:run_flappclient',
+        ]},
+
+        cmdclass = commands,
+
+        install_requires = [
+            'twisted[tls] >= 16.0.0',
+            'pyOpenSSL',
+            'six >= 1.12.0'
+        ],
+
+        # txi2p 0.3.2       -- Python 2 Only
+        # txsocksx 1.15.0.2 -- Python 2 Only
+
+        extras_require = {
+            'dev'  : ['mock', 'future >= 0.17.1', 'txtorcon >= 18.0.0'],
+            'socks': ['txsocksx'],
+            'tor'  : ['txtorcon >= 0.16.1'],
+            'i2p'  : ['txi2p >= 0.3.2'],
+        }
+    )
+
+
+if __name__ == '__main__':
+    setup()
